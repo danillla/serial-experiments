94bbc62f6920d1ed10e7101301014837b1112f1b
Daniil Alferov <daniil.alferov@apsis.com>
2021-03-26 16:42:46 +0300

    Add xcframework

---
+++ SerialExperiments.xcframework/ios-arm64_armv7/SerialExperiments.framework/Headers/SerialExperiments.h
@@ -0,0 +1,18 @@
+//
+//  SerialExperiments.h
+//  SerialExperiments
+//
+//  Created by Daniil Alferov on 26.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SerialExperiments.
+FOUNDATION_EXPORT double SerialExperimentsVersionNumber;
+
+//! Project version string for SerialExperiments.
+FOUNDATION_EXPORT const unsigned char SerialExperimentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SerialExperiments/PublicHeader.h>
+#import <SerialExperiments/SerialExperimentsAPI.h>
+
