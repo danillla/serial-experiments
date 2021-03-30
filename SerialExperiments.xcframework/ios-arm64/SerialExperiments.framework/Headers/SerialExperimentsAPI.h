20f42d7d28e2a56d04dfb15c06caa39bd2cb02ef
danillla <danillla@users.noreply.github.com>
2021-03-30 11:30:23 +0000

    Update framework

---
+++ SerialExperiments.xcframework/ios-arm64/SerialExperiments.framework/Headers/SerialExperimentsAPI.h
@@ -0,0 +1,22 @@
+//
+//  SerialExperimentsAPI.h
+//  SerialExperiments
+//
+//  Created by Daniil Alferov on 29.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SerialExperimentsAPI : NSObject
+
++ (void)experimentOne;
++ (void)experimentTwo;
++ (void)experimentThree;
++ (void)experimentFour;
++ (void)experimentFive;
+
+@end
+
+NS_ASSUME_NONNULL_END
