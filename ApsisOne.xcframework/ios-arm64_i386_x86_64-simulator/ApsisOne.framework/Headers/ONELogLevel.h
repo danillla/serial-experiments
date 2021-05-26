104fff4d0c0a538fb3b4fecb4fff903265f4fc36
danillla <danillla@users.noreply.github.com>
2021-05-26 14:12:20 +0000

    Update framework

---
+++ ApsisOne.xcframework/ios-arm64_i386_x86_64-simulator/ApsisOne.framework/Headers/ONELogLevel.h
@@ -0,0 +1,19 @@
+//
+//  ONELogLevel.h
+//  ApsisOne
+//
+//  Created by Daniil Alferov on 11.05.2021.
+//  Copyright © 2021 APSIS International AB. All rights reserved.
+//
+
+#ifndef ONELogLevel_h
+#define ONELogLevel_h
+
+typedef NS_ENUM(NSInteger, ONELogLevel) {
+    ONELogLevelInfo = 1,
+    ONELogLevelWarning = 2,
+    ONELogLevelError = 3,
+    ONELogLevelNone = 10
+};
+
+#endif /* ONELogLevel_h */
